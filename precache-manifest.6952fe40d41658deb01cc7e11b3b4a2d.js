f5bddfb60ff6857e5d25d286ce8642db172590c6
jmigsdj <miggydj@gmail.com>
2019-02-26 11:58:37 +0800

    Updates

---
+++ precache-manifest.6952fe40d41658deb01cc7e11b3b4a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cde00616bb4e3308c99c",
+    "url": "/my-website/static/js/runtime~main.cde00616.js"
+  },
+  {
+    "revision": "145d6dbe9896a9a78a75",
+    "url": "/my-website/static/js/main.145d6dbe.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/my-website/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "145d6dbe9896a9a78a75",
+    "url": "/my-website/static/css/main.ad945469.chunk.css"
+  },
+  {
+    "revision": "0999ec51fdb18768dc331a67d50f3223",
+    "url": "/my-website/index.html"
+  }
+];
